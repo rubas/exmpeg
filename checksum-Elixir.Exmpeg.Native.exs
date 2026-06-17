f3ec1b94a7e1edf5149bade1246ad787e6b0c30e
Dominic Rubas <1042243+rubas@users.noreply.github.com>
2026-06-17 23:18:36 +0200

    chore: refresh precompiled NIF checksums for v0.4.1

--- checksum-Elixir.Exmpeg.Native.exs
+++ checksum-Elixir.Exmpeg.Native.exs
@@ -1,5 +1,5 @@
 %{
-  "libexmpeg_native-v0.4.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:369272d24d3c1de0ec563ab5fd11ff926c5e7dd91052f3287caf223281a68132",
-  "libexmpeg_native-v0.4.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:3a697ce42a2f8b5d5a8b81206e45d6adf63a4ac9427077fd594d55fd73046144",
-  "libexmpeg_native-v0.4.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:aaff975af8e1cd43d3c8e3c6e57743d56d6cb4892171a0c72397b5be340202e7",
+  "libexmpeg_native-v0.4.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:0495ca6fbf4f333d8213550be03b779cb6c1729c2bca05555d0841dc122884fb",
+  "libexmpeg_native-v0.4.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:3aa4e4005b91f4987bd8c83d431fca633286ab65b069260b2b42f761f7f70449",
+  "libexmpeg_native-v0.4.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:24c68eb0f4787b51c6596e01a5f74ef3c762ec2c615590672464bd107df504f8",
 }
